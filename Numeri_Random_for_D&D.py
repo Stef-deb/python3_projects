e30a1156255db2e7edafd0381ca113af1b741758
Stef-deb <68242764+Stef-deb@users.noreply.github.com>
2020-07-16 19:06:04 +0200

    Update Numeri_Random_for_D&D.py

--- Numeri_Random_for_D&D.py
+++ Numeri_Random_for_D&D.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from kivymd.app import App
+from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
