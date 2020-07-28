099f80f11cc00afbae9bfc045a7b571e6e1a002e
Stef-deb <68242764+Stef-deb@users.noreply.github.com>
2020-07-28 16:56:00 +0200

    Update Numeri_Random_for_D&D.py

--- Numeri_Random_for_D&D.py
+++ Numeri_Random_for_D&D.py
@@ -3,34 +3,154 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Color, Rectangle
 from random import randint
+from kivy.core.window import Window
+from kivy.core.text import Label as CoreLabel
+from kivy.clock import Clock
+from functools import partial
+from kivy.core.audio import SoundLoader
+from kivy.animation import Animation
+from kivy.properties import ListProperty
+from kivy.uix.relativelayout import RelativeLayout
 
-lista = "numeri : "
+a = 0
+b = 0
+B = 0
+A = [1]
+var = []
+var2 = []
+clock = [0,0]
+clock2 = [0,0]
+lista = "Numeri : "
 somma = []
-hai_selezionato = "hai selezionato: "
+hai_selezionato = "Hai selezionato: "
+posiz = Window.size[0]*-1
+posiz1= int(Window.size[0]*0.45)
+#controllo statico risoluzione
+
+#var = Window.size[1]
+#clock = [0,var*0.915]
+#print(clock)
+
+#var1 = Window.size[0]
+#clockx = [var,0]
+#print(clock)
+
 
 
 class MainApp(FloatLayout):
-
     def __init__(self,*args):
         super().__init__(*args)
-        self.hint_label = Label(text="Tira i dadi!",font_size=24, pos= (0, 280))
-        self.main_label = Label(text="",font_size=14, text_size=((self.width --180),(None)), pos= (0, 50))
-        self.second_label = Label(text= "" ,font_size=16, pos= (0,180))
-        self.third_label = Label(text="",font_size=14,  text_size=((self.width --180),(None)), pos= (0, -90))
+        self.add_widget(self.Background())
+        self.add_widget(self.Toolbar())
+        self.hint_label = Label(text="Tira i dadi!",font_size=50,outline_width= 2, outline_color= (0,0,0,1), pos_hint= {"x":0, "y":0.37})
+        self.main_label = Label(text="",font_size=32, text_size=((self.width --580),(None)),outline_width= 2, outline_color= (0,0,0,1), pos= (-8, 140))
+        self.second_label = Label(text= "" ,font_size=32, pos= (-13,480),outline_width= 2, outline_color= (0,0,0,1))
+        self.third_label = Label(text="",font_size=32,  text_size=((self.width --580),(None)),outline_width= 2, outline_color= (0,0,0,1), pos= (-8, -260))
         self.add_widget(self.hint_label)
         self.add_widget(self.main_label)
         self.add_widget(self.second_label)
         self.add_widget(self.third_label)
-        self.add_widget(Button(text="D-4",font_size=10, pos= (30, 38), size_hint= (0.1, 0.1), on_press=self.D4))
-        self.add_widget(Button(text="D-6",font_size=10, pos= (70, 38), size_hint= (0.1, 0.1), on_press=self.D6))
-        self.add_widget(Button(text="D-8",font_size=10, pos= (110, 38), size_hint= (0.1, 0.1), on_press=self.D8))
-        self.add_widget(Button(text="D-10",font_size=10, pos= (150, 38), size_hint= (0.1, 0.1), on_press=self.D10))
-        self.add_widget(Button(text="D-12",font_size=10, pos= (190, 38), size_hint= (0.1, 0.1), on_press=self.D12))
-        self.add_widget(Button(text="D-20",font_size=10, pos= (230, 38), size_hint= (0.1, 0.1), on_press=self.D20))
-        self.add_widget(Button(text="D-100",font_size=10, pos= (270, 38), size_hint= (0.1, 0.1), on_press=self.D100))
-        self.add_widget(Button(text="clear",font_size=10, pos= (280, 500), size_hint= (0.1, 0.1), on_press=self.clear ))
+        self.add_widget(Label(text="Dadi per D & D",font_size=58,outline_width= 2, outline_color= (0,0,0,1), pos_hint= {"x":0, "y":0.46222}))
+#buttons
 
+        self.add_widget(Button(background_down= "img/d4_light.png",background_normal= "img/d4.png", pos_hint= {"x":0.0325, "y":0.03267},size_hint= (0.1155, 0.07), on_press=self.D4))
+        self.add_widget(Button(background_down= "img/d6_light.png",background_normal= "img/d6.png", pos_hint= {"x":0.169, "y":0.03267}, size_hint= (0.1155, 0.07), on_press=self.D6))
+        self.add_widget(Button(background_down= "img/d8_light.png",background_normal= "img/d8.png", pos_hint= {"x":0.3055, "y":0.03267}, size_hint= (0.1155, 0.07), on_press=self.D8))
+        self.add_widget(Button(background_down= "img/d10_light.png",background_normal= "img/d10.png",pos_hint= {"x":0.442, "y":0.03267}, size_hint= (0.1155, 0.07), on_press=self.D10))
+        self.add_widget(Button(background_down= "img/d12_light.png",background_normal= "img/d12.png",pos_hint= {"x":0.5785, "y":0.03267}, size_hint= (0.1155, 0.07), on_press=self.D12))
+        self.add_widget(Button(background_down= "img/d20_light.png",background_normal= "img/d20.png",pos_hint= {"x":0.715, "y":0.03267}, size_hint= (0.1155, 0.07), on_press=self.D20))
+        self.add_widget(Button(background_down= "img/d100_light.png",background_normal= "img/d100.png",pos_hint= {"x":0.8515, "y":0.03267}, size_hint= (0.1155, 0.07), on_press=self.D100))
+        self.add_widget(Button(text="clear",font_size=32,pos_hint= {"x":0.8066,"y": 0.922}, size_hint= (0.1155, 0.07), on_press=self.clear ))
+        self.add_widget(Button(background_normal= "img/toolbutton.png",pos_hint= {"x":0.0333, "y":0.92222}, size_hint= (0.2, 0.07), on_press=self.Tend))
+
+#testi dei buttons / buttons texts
+
+        self.d4t = Label(text="D-4", font_size=32, outline_width= 2,pos_hint= {"x":-0.41111, "y":-0.43067}, outline_color= (0,0,0,1))
+        self.d6t = Label(text="D-6", font_size=32, outline_width= 2,pos_hint= {"x":-0.27461, "y":-0.43067}, outline_color= (0,0,0,1))
+        self.d8t = Label(text="D-8", font_size=32, outline_width= 2,pos_hint= {"x":-0.13811, "y":-0.43067}, outline_color= (0,0,0,1))
+        self.d10t = Label(text="D-10", font_size=32, outline_width= 2,pos_hint= {"x":-0.00161, "y":-0.43067}, outline_color= (0,0,0,1))
+        self.d12t = Label(text="D-12", font_size=32, outline_width= 2,pos_hint= {"x":0.13489, "y":-0.43067}, outline_color= (0,0,0,1))
+        self.d20t = Label(text="D-20", font_size=32, outline_width= 2,pos_hint= {"x":0.27139, "y":-0.43067}, outline_color= (0,0,0,1))
+        self.d100t = Label(text="D-100", font_size=32, outline_width= 2,pos_hint= {"x":0.40989, "y":-0.43067}, outline_color= (0,0,0,1))
+        self.add_widget(self.d4t)
+        self.add_widget(self.d6t)
+        self.add_widget(self.d8t)
+        self.add_widget(self.d10t)
+        self.add_widget(self.d12t)
+        self.add_widget(self.d20t)
+        self.add_widget(self.d100t)
+        self.add_widget(var1)
+
+#menu
+
+
+
+    class Toolbar(Label):
+        def __init__(self,*args):
+            super().__init__(*args)
+            with self.canvas.before:
+                Color(255/255,85/255,13/255,1) #RGB 93, 220, 196
+                self.rectangled= Rectangle(pos=clock,size=(Window.size))
+            self.bind(pos=self.update_rect, size=self.update_rect)
+        def update_rect(self,*args):
+            global clock
+            self.rectangled.pos= clock
+            self.rectangled.size= Window.size
+
+    class Background(Label):
+        def __init__(self,*args):
+            super().__init__(*args)
+            with self.canvas.before:
+                Color(102/255,255/255,133/255,1) #RGB 93, 220, 196
+                self.rectangled1= Rectangle(size=(Window.size))
+            self.bind( size=self.update_rect1)
+        def update_rect1(self,*args):
+            self.rectangled1.size= Window.size
+
+
+#uscita menu
+
+    pression_2 = ListProperty([0, 0])
+
+    def Tend(self,*args):
+        global A, var1, posiz1
+        if var1.pos[0] == posiz1 -1 or var1.pos[0] == 0:
+            self.pression_2 = A
+            A += A
+            return A
+            return True
+            return super(CustomBtn, self).on_touch_down(touch)
+
+    def rip3(self,*args):
+        global var1
+        global a,b
+        a += 1
+        b += 1
+        var1.pos = (a,0)
+        print(var1.pos[0])
+
+    def rip4(self,*args):
+        global var1
+        global a,b
+        a -= 1
+        b -= 1
+        var1.pos = (a,0)
+        print(var1.pos[0])
+
+
+    def on_pression_2(self, instance ,pos):
+        global posiz1
+        if a == 0:
+            for i in range (1,posiz1):
+                Clock.schedule_once(partial(self.rip3),i*0.0009)
+        else:
+            for i in range (1,posiz1):
+                Clock.schedule_once(partial(self.rip4),i*0.0009)
+
+
+# roll dei dadi
 
     def D4(self,*args):
         global lista
@@ -50,7 +170,7 @@ class MainApp(FloatLayout):
         self.second_label.text = "totale: " + somma1
         hai_selezionato += " -D4"
         self.third_label.text = hai_selezionato
-        if len(lista) >= 500:
+        if len(lista) >= 600:
             lista = "numeri: "
             somma = []
             hai_selezionato = "hai selezionato: "
@@ -76,7 +196,7 @@ class MainApp(FloatLayout):
         self.second_label.text = "totale: " + somma1
         hai_selezionato += " -D6"
         self.third_label.text = hai_selezionato
-        if len(lista) >= 500:
+        if len(lista) >= 600:
             lista = "numeri: "
             somma = []
             hai_selezionato = "hai selezionato: "
@@ -102,7 +222,7 @@ class MainApp(FloatLayout):
         self.second_label.text = "totale: " + somma1
         hai_selezionato += " -D8"
         self.third_label.text = hai_selezionato
-        if len(lista) >= 500:
+        if len(lista) >= 600:
             lista = "numeri: "
             somma = []
             hai_selezionato = "hai selezionato: "
@@ -128,7 +248,7 @@ class MainApp(FloatLayout):
         self.second_label.text = "totale: " + somma1
         hai_selezionato += " -10"
         self.third_label.text = hai_selezionato
-        if len(lista) >= 450:
+        if len(lista) >= 550:
             lista = "numeri: "
             somma = []
             hai_selezionato = "hai selezionato: "
@@ -154,7 +274,7 @@ class MainApp(FloatLayout):
         self.second_label.text = "totale: " + somma1
         hai_selezionato += " -D12"
         self.third_label.text = hai_selezionato
-        if len(lista) >= 450:
+        if len(lista) >= 550:
             lista = "numeri: "
             somma = []
             hai_selezionato = "hai selezionato: "
@@ -180,7 +300,7 @@ class MainApp(FloatLayout):
         self.second_label.text = "totale: " + somma1
         hai_selezionato += " -D20"
         self.third_label.text = hai_selezionato
-        if len(lista) >= 450:
+        if len(lista) >= 550:
             lista = "numeri: "
             somma = []
             hai_selezionato = "hai selezionato: "
@@ -208,7 +328,7 @@ class MainApp(FloatLayout):
         self.second_label.text = "totale: " + somma1
         hai_selezionato += " -D100"
         self.third_label.text = hai_selezionato
-        if len(lista) >= 450:
+        if len(lista) >= 550:
             lista = "numeri: "
             somma = []
             hai_selezionato = "hai selezionato: "
@@ -220,18 +340,45 @@ class MainApp(FloatLayout):
         global lista
         global somma
         global hai_selezionato
-        lista = "numeri: "
+        lista = "Numeri: "
         somma = []
-        hai_selezionato = "hai selezionato: "
+        hai_selezionato = "Hai selezionato: "
         self.main_label.text = ""
         self.second_label.text = ""
         self.third_label.text = ""
+
+    def contr(self):
+        global var
+        global clock
+        var = Window.size[1]
+        clock = [0,var*0.915]
+        #print(clock)
+
+    Clock.schedule_interval(partial(contr),0.001)
+
+
+
+
+class Stiic(RelativeLayout):
+    global A,B,posiz
+    def __init__(self,*args):
+        super().__init__(*args)
+        self.add_widget(Button(text= ("premi "),size_hint = (0.1155, 0.07),pos_hint ={"center_x": -0.1, "center_y": 0.5}))
+        with self.canvas.before:
+            Color(1,1,1,1) #RGB 93, 220, 196
+            self.rectanglez = Rectangle(pos=(posiz,0),size=(Window.size))
+
+    pression_1 = ListProperty([0, 0])
+
+
+
+var1 = Stiic()
 
 
 
 class Numeri(App):
     def build(self):
         return MainApp()
-If __name__ == __"main"__:
+if __name__ == "__main__":
 
-    Numeri().run()
+	Numeri().run()
